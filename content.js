082342872a12f9463c447a315ee967dd0653ee6c
chengyifan <chengyifan@taou.com>
2023-02-10 14:39:18 +0800

    v1

---
+++ content.js
@@ -0,0 +1,43 @@
+function execute(filePath) {
+  const script = document.createElement("script");
+  script.src = chrome.runtime.getURL(filePath);
+  document.documentElement.appendChild(script);
+  script.parentNode.removeChild(script);
+}
+
+execute("./webpackhook.js");
+execute("./mg-file.js");
+
+function onShow(selector, cb) {
+  let visible = false;
+  setInterval(() => {
+    const el = document.querySelector(selector);
+    if (!visible && el) {
+      visible = true;
+      cb(el);
+    } else if (visible && !el) {
+      visible = false;
+    }
+  }, 400);
+}
+
+function run() {
+  onShow(".right-export-bar", () => {
+    const btnCon = document.querySelector(".right-export-bar");
+    const preview = document.querySelector(".preview");
+    preview.style["gridArea"] = "auto";
+    const btn = document.createElement("button");
+    btn.setAttribute(
+      "class",
+      "export-button m-button m-button--xs m-button--highlight"
+    );
+    btn.style.marginTop = "8px";
+    btn.innerHTML = '<span class="m-button__content"> 导出CDN </span>';
+    btn.onclick = function () {
+      execute("./export.js");
+    };
+    btnCon.insertBefore(btn, preview);
+  });
+}
+
+window.addEventListener("load", run);
