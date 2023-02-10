710434a99d7626ae5cdd5e3d088d20a674befbdc
chengyifan <chengyifan@taou.com>
2023-02-10 15:39:49 +0800

    fix

--- ui.js
+++ ui.js
@@ -1,3 +1,35 @@
+function exportAndUpload() {
+  const toast = window.mgfile.$toast({
+    content: "正在上传",
+    persistent: !0,
+    disabledTransition: !0,
+    position: "bottom",
+  });
+
+  const file = window.mgfile.getExportList()[0];
+  const formData = new FormData();
+  formData.append("quality", "0.6-0.8");
+  formData.append("file", new Blob([file.buffer]), "export.png");
+
+  fetch("https://growth-bi-service-fe.in.taou.com/upload/cdn/", {
+    method: "POST",
+    body: formData,
+  }).then((resp) => {
+    resp.json().then((data) => {
+      toast.clearToast();
+      navigator.clipboard.writeText(data.file).then(() => {
+        window.mgfile.$toast({
+          content: "上传成功，已复制 URL 到剪切板",
+          position: "bottom",
+        });
+      });
+    });
+  });
+  fetch(
+    "https://maimai.cn/n/platform/api/public/news?e=lanhu_upload_cdn"
+  ).catch((e) => {});
+}
+
 window.uploadCDNInit = function () {
   const btnCon = document.querySelector(".right-export-bar");
   const preview = document.querySelector(".preview");
@@ -9,8 +41,6 @@ window.uploadCDNInit = function () {
   );
   btn.style.marginTop = "8px";
   btn.innerHTML = '<span class="m-button__content"> 导出到CDN </span>';
-  btn.onclick = function () {
-    execute("./export.js");
-  };
+  btn.onclick = exportAndUpload;
   btnCon.insertBefore(btn, preview);
 };
